ec42e226fea77aef64bc24738d20762d1e823794
Roee Shapira <roee.shapira@tensorleap.ai>
2022-12-25 15:18:01 +0200

    Add scanner

---
+++ src/lib/lexer/scan.ts
@@ -0,0 +1,43 @@
+import { type TTokenCategories } from "./tokens.ts";
+import {
+	detectKeyword,
+	detectLiteral,
+	detectMisc,
+	detectOperators,
+	detectPunctuation,
+	type TDetectionResult,
+} from "./detectors.ts";
+
+const DETECTORS = [
+	detectOperators,
+	detectKeyword,
+	detectLiteral,
+	detectMisc,
+	detectPunctuation,
+] as const;
+
+export type TAnyDetectionResult = {
+	[K in TTokenCategories]: Omit<TDetectionResult<K>, "tail"> & { position: number };
+}[TTokenCategories];
+
+export function* scan(
+	input: string,
+): Generator<TAnyDetectionResult> {
+	let currentInput = input;
+	let position = 0;
+
+	while (currentInput) {
+		for (const detector of DETECTORS) {
+			const result = detector(currentInput);
+
+			if (result) {
+				const { tail, ...tokenValue } = result;
+				yield { ...tokenValue, position };
+
+				currentInput = tail;
+				position += tokenValue.value.length;
+				break;
+			}
+		}
+	}
+}
