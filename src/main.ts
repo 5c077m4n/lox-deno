5efbd98f77ccc1460b7cd0bd30909c76cafed1b8
Roee Shapira <roee.shapira@tensorleap.ai>
2022-12-26 21:42:00 +0200

    Add parsing and printing to CLI options

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,45 @@
 import { parse } from "flags";
 
 import { scan } from "./lib/lexer/scan.ts";
+import { Parser } from "./lib/parser/parser.ts";
+import { Parenthesize } from "./lib/parser/visitors/paren.ts";
+
+const PRINTER = new Parenthesize();
 
 async function main() {
-	const { _: fileName, eval: evaluate } = parse(Deno.args, { string: ["eval"] });
+	const { _: fileName, eval: evaluate, printAst } = parse(Deno.args, {
+		string: ["eval"],
+		boolean: ["printAst"],
+		default: { printAst: false },
+	});
 
 	if (fileName?.length) {
 		const fileContent = await Deno.readTextFile(fileName.join(" "));
-		const tokens = [...scan(fileContent)];
-		console.log(tokens);
+		const tokens = scan(fileContent);
+		const parser = new Parser(tokens);
+		const expr = parser.parse();
+
+		if (parser.getErrors().length) {
+			console.error(parser.getErrors());
+		}
+
+		if (printAst && expr) {
+			const paren = PRINTER.print(expr);
+			console.log(paren);
+		}
 	} else if (evaluate && typeof evaluate === "string") {
-		const tokens = [...scan(evaluate)];
-		console.log(tokens);
+		const tokens = scan(evaluate);
+		const parser = new Parser(tokens);
+		const expr = parser.parse();
+
+		if (parser.getErrors().length) {
+			console.error(parser.getErrors());
+		}
+
+		if (printAst && expr) {
+			const paren = PRINTER.print(expr);
+			console.log(paren);
+		}
 	}
 }
 
